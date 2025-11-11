642a6a987d0a66894d069df44111304f894a68a5
Ростислав Грачев <gracevrostislav5@gmail.com>
2025-11-11 13:30:24 +0700

    Закончил с заданиями на редактирование формы администратора

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -7,6 +7,7 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+
 class Language(models.Model):
     name = models.CharField(max_length=200, help_text="Enter a book languages")
     def __str__(self):
@@ -25,6 +26,12 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
